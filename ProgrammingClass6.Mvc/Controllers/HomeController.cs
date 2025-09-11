60c671aca01c5516df2e489ad3919fc3678ba4ea
Armadon <movsisyan191287@gmail.com>
2025-09-11 12:52:37 +0400

    MVC Lesson one: First commit: Moved from js-html to MVC structure

--- ProgrammingClass6.Mvc/Controllers/HomeController.cs
+++ ProgrammingClass6.Mvc/Controllers/HomeController.cs
@@ -23,6 +23,21 @@ namespace ProgrammingClass6.Mvc.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
