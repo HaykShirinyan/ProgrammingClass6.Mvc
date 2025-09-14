c8b1d0774ddfa17be1cea77268ba322b4366252a
levonavetisyan409 <levonavetisyan032@gmail.com>
2025-09-14 05:32:35 -0700

    Update ApplicationDbContext.cs

--- ProgrammingClass6.Mvc/Data/ApplicationDbContext.cs
+++ ProgrammingClass6.Mvc/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProgrammingClass6.Mvc.Models;
 
 namespace ProgrammingClass6.Mvc.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Product> Products {get; set;}
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
