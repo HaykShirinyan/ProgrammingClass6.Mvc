05ad3bb8ac3bc8d1ea6c0835b9289c552dbb238c
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2025-09-14 12:47:13 -0700

    Added Create and Edit product pages

--- ProgrammingClass6.Mvc/Controllers/ProductsController.cs
+++ ProgrammingClass6.Mvc/Controllers/ProductsController.cs
@@ -13,11 +13,57 @@ namespace ProgrammingClass6.Mvc.Controllers
             _dbContext = dbContext;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             List<Product> products = _dbContext.Products.ToList();
 
             return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Products.Add(product);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // /products/edit/{id}
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _dbContext
+                .Products
+                .SingleOrDefault(dbProductRow => dbProductRow.Id == id);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Products.Update(product);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
         }
     }
 }
