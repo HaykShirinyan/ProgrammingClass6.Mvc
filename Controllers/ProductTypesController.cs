8f4029b8f96a70d5cb9ae2188c51a968171a8b34
Karen Hayrapetyan <karendyan@mail.ru>
2025-09-17 13:53:40 +0400

    Add ProductTypes

--- Controllers/ProductTypesController.cs
+++ Controllers/ProductTypesController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 using ProgrammingClass6.Mvc.Data;
 using ProgrammingClass6.Mvc.Models;
+using System.Collections.Generic;
+using ProgrammingClass6.Mvc.Migrations;
+using ProductType = ProgrammingClass6.Mvc.Models.ProductType;
 namespace ProgrammingClass6.Mvc.Controllers
 {
     public class ProductTypesController : Controller
@@ -11,12 +15,52 @@ namespace ProgrammingClass6.Mvc.Controllers
             _context = context;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             List<ProductType> productTypes = _context.ProductTypes.ToList();
          
             return View(productTypes);
         }
-             
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Creat(ProductType productType)
+        { 
+        if (ModelState.IsValid)
+            {
+                _context.ProductTypes.Add(productType);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+
+            [HttpGet]
+            Public IActionResult Edit(int id)
+            {
+                var productType = _context
+                    .ProductTypes.SingleOrDefault(pt => pt.Id == id);
+                return View(productType);
+            }
+
+            [HttpPost]
+            Public IActionResult Edit(ProductType productType)
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.ProductTypes.Update(productType);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(productType);
+            }
+        }
+
     }
 }
