e7c06e9646ea96da99cf4dc0d5e0c4a74abeaf66
Melanya Arakelyan <melanya.arakelyan.a@gmail.com>
2025-09-21 02:14:14 +0400

    HWgithub1

--- ProgrammingClass6.Mvc/Controllers/HomeController.cs
+++ ProgrammingClass6.Mvc/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ProgrammingClass6.Mvc.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
