99e6c327e501266a79f1bd875213608442be46b4
levonavetisyan409 <levonavetisyan032@gmail.com>
2025-09-21 04:05:37 -0700

    Update ProductsCotnroller.cs

--- ProgrammingClass6.Mvc/Controllers/ProductsCotnroller.cs
+++ ProgrammingClass6.Mvc/Controllers/ProductsCotnroller.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProgrammingClass6.Mvc.Data;
+using ProgrammingClass6.Mvc.Models;
 
 namespace ProgrammingClass6.Mvc.Controllers
 {
@@ -15,6 +16,41 @@ namespace ProgrammingClass6.Mvc.Controllers
 		{
 			var products = _dbContext.Products.ToList();
 			return View(products);
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Product product)
+		{
+			_dbContext.Products.Add(product);
+			_dbContext.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Edit(int id)
+		{
+			var product = _dbContext
+				.Products
+				.FirstOrDefault(p=> p.Id == id);
+
+			return View(product);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Product product)
+		{
+			_dbContext.Products.Update(product);
+			_dbContext.SaveChanges();
+
+			return RedirectToAction("Index");
 		}
 	}
 }
