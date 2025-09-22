141887105a1f1d06f134b592a5ea31195559618d
Armadon <movsisyan191287@gmail.com>
2025-09-22 18:48:19 +0400

    Added: Product create edit

--- ProgrammingClass6.Mvc/Controllers/ProductsController.cs
+++ ProgrammingClass6.Mvc/Controllers/ProductsController.cs
@@ -13,11 +13,61 @@ namespace ProgrammingClass6.Mvc.Controllers
             _dbContext = dbContext;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             List<Product> products = _dbContext.Products.ToList();
 
             return View(products);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Product product)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Products.Add(product);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Product product = _dbContext.Products.SingleOrDefault(p => p.Id == id);
+
+            return View(product);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Products.Update(product);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
         }
     }
 }
