11ad4cf1591e900fafc4409e923aca697081663c
Levon Avetisyan <levonavetisyan032@gmail.com>
2025-10-05 07:02:39 -0700

    Add files via upload

---
+++ ProgrammingClass6.Mvc/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProgrammingClass6.Mvc.Data;
+using ProgrammingClass6.Mvc.Models;
+
+namespace ProgrammingClass6.Mvc.Controllers
+{
+    public class ProductCategoriesController : Controller
+    {
+        private ApplicationDbContext _dbCotnext;
+
+        public ProductCategoriesController(ApplicationDbContext dbCotnext)
+        {
+            _dbCotnext = dbCotnext; 
+        }
+
+        [HttpGet]
+        public IActionResult Index(int productId)
+        {
+            ViewBag.ProductId = productId;
+
+            var productCategories = _dbCotnext
+                .ProductCategories
+                .Include(productCategory => productCategory.Category)
+                .Where(productCategory => productCategory.ProductId == productId)
+                .ToList();
+
+            return View(productCategories);
+        }
+
+        [HttpGet]
+        public IActionResult Create(int productId)
+        {
+            var productCategory = new ProductCategory();
+
+            productCategory.ProductId = productId;
+
+            ViewBag.Categories = _dbCotnext.Categories.ToList();
+
+            return View(productCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductCategory productCategory)
+        {
+            _dbCotnext.ProductCategories.Add(productCategory);
+            _dbCotnext.SaveChanges();
+
+            return RedirectToAction("Index", new { productId = productCategory.ProductId });
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int productId, int categoryId)
+        {
+            var productCategory = _dbCotnext
+                .ProductCategories
+                .SingleOrDefault(productCategory => productCategory.ProductId == productId && productCategory.CategoryId == categoryId);
+
+            _dbCotnext.Remove(productCategory);
+            _dbCotnext.SaveChanges();
+
+            return RedirectToAction("Index", new { productId = productCategory.ProductId });
+        }
+    }
+}
