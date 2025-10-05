4f405366d67b6306741c8e2ae8e2310910eae09b
Levon Avetisyan <levonavetisyan032@gmail.com>
2025-10-05 07:00:36 -0700

    Update ApplicationDbContext.cs

--- ProgrammingClass6.Mvc/Data/ApplicationDbContext.cs
+++ ProgrammingClass6.Mvc/Data/ApplicationDbContext.cs
@@ -6,10 +6,26 @@ namespace ProgrammingClass6.Mvc.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public DbSet<Product> Products {get; set;}
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder
+                .Entity<ProductCategory>()
+                .HasKey(productCategory => new { productCategory.ProductId, productCategory.CategoryId });
         }
     }
 }
