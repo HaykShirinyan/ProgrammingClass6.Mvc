6865b84309c48262c96dae416ef324318ca6943d
A-rmen <armenhovhannisyannn@gmail.com>
2025-10-17 01:07:53 +0400

    I build Add and Edit

--- ProgrammingClass6.Mvc/Controllers/ProductTypesController.cs
+++ ProgrammingClass6.Mvc/Controllers/ProductTypesController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore.Metadata;
 using ProgrammingClass6.Mvc.Data;
 using ProgrammingClass6.Mvc.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ProgrammingClass6.Mvc.Controllers
 {
@@ -17,6 +20,46 @@ namespace ProgrammingClass6.Mvc.Controllers
             List<ProductType> productTypes = _dbContext.ProductTypes.ToList();
 
             return View(productTypes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.ProductTypes.Add(productType);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var productType = _dbContext
+                .ProductTypes
+                .SingleOrDefault(dbProductTypeRow => dbProductTypeRow.Id == id);
+            return View(productType);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.ProductTypes.Update(productType);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productType);
         }
     }
 }
--- ProgrammingClass6.Mvc/Controllers/UnitOfMeasuresController.cs
+++ ProgrammingClass6.Mvc/Controllers/UnitOfMeasuresController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProgrammingClass6.Mvc.Data;
-using ProgrammingClass6.Mvc.Models; 
+using ProgrammingClass6.Mvc.Models;
 
 namespace ProgrammingClass6.Mvc.Controllers
 {
@@ -18,6 +18,47 @@ namespace ProgrammingClass6.Mvc.Controllers
             List<UnitOfMeasure> unitOfMeasures = _dbContext.UnitOfMeasures.ToList();
 
             return View(unitOfMeasures);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UnitOfMeasure unitOfMeasure)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.UnitOfMeasures.Add(unitOfMeasure);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var unitOfMeasure = _dbContext
+                .UnitOfMeasures
+                .SingleOrDefault(dbUnitOfMeasureRow => dbUnitOfMeasureRow.Id == id);
+            return View(unitOfMeasure);
+         }
+
+
+        [HttpPost]
+        public IActionResult Edit(UnitOfMeasure unitOfMeasure)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.UnitOfMeasures.Update(unitOfMeasure);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(unitOfMeasure);
         }
     }
 }
