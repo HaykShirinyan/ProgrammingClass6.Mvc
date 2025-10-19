9c1d00cee86d59f203b7b14d5fb64958158358d0
Levon Avetisyan <levonavetisyan032@gmail.com>
2025-10-19 09:37:10 -0700

    Delete ProgrammingClass6.Mvc directory

--- ProgrammingClass6.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProgrammingClass6.Mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
